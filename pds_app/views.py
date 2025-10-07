06a3f8b3a6316a7547023135c13ca2a183beb53c
Gabrielkaos <gabrielmontes155@gmail.com>
2025-10-07 17:02:00 +0800

    full edit_form.html

--- pds_app/views.py
+++ pds_app/views.py
@@ -592,7 +592,87 @@ def edit_form(request, form_id):
             education_form.save()
             other_form.save()
             print("Saved")
-            
+
+            # save civil service eligibility
+            # Delete existing civil service records
+            form_instance.civil_service.all().delete()
+
+            # Save new civil service records
+            career_services = request.POST.getlist('career_service[]')
+            civil_services = []
+            for i in range(len(career_services)):
+                if career_services[i].strip():
+                    civil_service = CivilServiceEligibility.objects.create(
+                        user=request.user,
+                        career_service=career_services[i],
+                        rating=request.POST.getlist('rating[]')[i] if i < len(request.POST.getlist('rating[]')) else None,
+                        exam_date=request.POST.getlist('exam_date[]')[i] if i < len(request.POST.getlist('exam_date[]')) else None,
+                        exam_place=request.POST.getlist('exam_place[]')[i],
+                        license_number=request.POST.getlist('license_number[]')[i],
+                        license_validity=request.POST.getlist('license_validity[]')[i] if i < len(request.POST.getlist('license_validity[]')) else None,
+                    )
+                    civil_services.append(civil_service)
+
+            # Connect to CompleteForm
+            form_instance.civil_service.set(civil_services)
+
+
+            # Delete and recreate Work Experience
+            form_instance.work_experience.all().delete()
+            work_from_dates = request.POST.getlist('work_from_date[]')
+            work_experiences = []
+            for i in range(len(work_from_dates)):
+                if work_from_dates[i]:
+                    work_exp = WorkExperience.objects.create(
+                        user=request.user,
+                        from_date=work_from_dates[i],
+                        to_date=request.POST.getlist('work_to_date[]')[i] if i < len(request.POST.getlist('work_to_date[]')) and request.POST.getlist('work_to_date[]')[i] else None,
+                        position_title=request.POST.getlist('work_position_title[]')[i] if i < len(request.POST.getlist('work_position_title[]')) else '',
+                        department=request.POST.getlist('work_department[]')[i] if i < len(request.POST.getlist('work_department[]')) else '',
+                        monthly_salary=request.POST.getlist('work_monthly_salary[]')[i] if i < len(request.POST.getlist('work_monthly_salary[]')) and request.POST.getlist('work_monthly_salary[]')[i] else None,
+                        salary_grade=request.POST.getlist('work_salary_grade[]')[i] if i < len(request.POST.getlist('work_salary_grade[]')) else None,
+                        status_of_appointment=request.POST.getlist('work_status[]')[i] if i < len(request.POST.getlist('work_status[]')) else '',
+                        govt_service=request.POST.getlist('work_govt_service[]')[i] if i < len(request.POST.getlist('work_govt_service[]')) else 'N',
+                    )
+                    work_experiences.append(work_exp)
+            form_instance.work_experience.set(work_experiences)
+
+            # Delete and recreate Voluntary Work
+            form_instance.voluntary_work.all().delete()
+            voluntary_orgs = request.POST.getlist('voluntary_org[]')
+            voluntary_works = []
+            for i in range(len(voluntary_orgs)):
+                if voluntary_orgs[i].strip():
+                    voluntary = VoluntaryWork.objects.create(
+                        user=request.user,
+                        organization_name=voluntary_orgs[i],
+                        from_date=request.POST.getlist('voluntary_from[]')[i] if i < len(request.POST.getlist('voluntary_from[]')) and request.POST.getlist('voluntary_from[]')[i] else None,
+                        to_date=request.POST.getlist('voluntary_to[]')[i] if i < len(request.POST.getlist('voluntary_to[]')) and request.POST.getlist('voluntary_to[]')[i] else None,
+                        number_of_hours=request.POST.getlist('voluntary_hours[]')[i] if i < len(request.POST.getlist('voluntary_hours[]')) and request.POST.getlist('voluntary_hours[]')[i] else 0,
+                        nature_of_work=request.POST.getlist('voluntary_nature[]')[i] if i < len(request.POST.getlist('voluntary_nature[]')) else '',
+                    )
+                    voluntary_works.append(voluntary)
+            form_instance.voluntary_work.set(voluntary_works)
+
+            # Delete and recreate Learning Development
+            form_instance.learning_development.all().delete()
+            learning_titles = request.POST.getlist('learning_title[]')
+            learning_developments = []
+            for i in range(len(learning_titles)):
+                if learning_titles[i].strip():
+                    learning = LearningDevelopment.objects.create(
+                        user=request.user,
+                        title=learning_titles[i],
+                        from_date=request.POST.getlist('learning_from[]')[i] if i < len(request.POST.getlist('learning_from[]')) and request.POST.getlist('learning_from[]')[i] else None,
+                        to_date=request.POST.getlist('learning_to[]')[i] if i < len(request.POST.getlist('learning_to[]')) and request.POST.getlist('learning_to[]')[i] else None,
+                        number_of_hours=request.POST.getlist('learning_hours[]')[i] if i < len(request.POST.getlist('learning_hours[]')) and request.POST.getlist('learning_hours[]')[i] else 0,
+                        type_of_ld=request.POST.getlist('learning_type[]')[i] if i < len(request.POST.getlist('learning_type[]')) else '',
+                        conducted_by=request.POST.getlist('learning_conducted[]')[i] if i < len(request.POST.getlist('learning_conducted[]')) else '',
+                    )
+                    learning_developments.append(learning)
+            form_instance.learning_development.set(learning_developments)
+
+                        
             # Update form name if provided
             if 'form_name' in request.POST:
                 form_instance.name = request.POST['form_name']
