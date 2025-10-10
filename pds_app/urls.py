e4719eb09c1e28046c525e95e759b99ee4fa81ad
Gabrielkaos <gabrielmontes155@gmail.com>
2025-10-10 19:11:25 +0800

    cleaned the project of useless lines and files

--- pds_app/urls.py
+++ pds_app/urls.py
@@ -6,13 +6,10 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('form/create_form/', views.create_form, name='create_form'),
-    path('edit_form/<int:form_id>/', views.edit_form, name='edit_form'),
+    path('form/edit_form/<int:form_id>/', views.edit_form, name='edit_form'),
     path('logout/', views.logout_view, name='logout'),
     path('login/', auth_views.LoginView.as_view(template_name='pds_app/login.html'), name='login'),
-    # path('form/<str:section>/', views.section_view, name='section_view'),
     path('', views.home, name='home'),
-    # path('create_form/', views.create_form, name='create_form'),
-    path('forms/', views.forms, name='forms'),
     path('forms/<int:form_id>', views.all_forms, name='all_forms'),
     path('delete_form/<int:form_id>', views.delete_form, name='delete_form'),
 ]
