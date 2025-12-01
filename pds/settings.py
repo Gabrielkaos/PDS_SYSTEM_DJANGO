06e439c8ba7736499812594b4d78b0a2a5a4ab5c
Gabrielkaos <gabrielmontes155@gmail.com>
2025-12-01 11:06:24 +0800

    migrated to mysql instead of sqlite3

--- pds/settings.py
+++ pds/settings.py
@@ -78,8 +78,15 @@ WSGI_APPLICATION = 'pds.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pds_db',
+        'USER':'pds_user',
+        'PASSWORD':'pds_password',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        }
     }
 }
 
